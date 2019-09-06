94c48a39f991d7dbe0efc854633f69bebfb14ace
arturpttp <arturfreitas089@gmail.com>
2019-09-06 19:31:56 -0300

    teste

--- git.sh
+++ git.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 ###########################
 git checkout master
-# add all added/modified files
 git add .
-# commit changes
 read commitMessage
-git commit -am "$commitMessage"
-# push to git remote repository
+git commit -m "$commitMessage"
 git push
 ###########################
 echo Press Enter...
